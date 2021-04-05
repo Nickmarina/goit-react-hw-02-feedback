14864905e49bdd8770237fd4a4f1ac22478d7a08
Marina Permiakova <marinapermyakova13@gmail.com>
2021-04-05 18:27:41 +0300

    add a message for statistics section

--- src/components/Statistics/index.jsx
+++ src/components/Statistics/index.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Notification from '../Notification';
 import styles from './styles.module.scss';
 
 const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad;
   return (
-    <ul className={styles.list}>
-      <li>Good: {good} </li>
-      <li>Neutral: {neutral} </li>
-      <li>Bad: {bad} </li>
-      <li>Total: {total} </li>
-      <li>Positive feedback: {Math.floor((good / total) * 100) || 0}%</li>
-    </ul>
+    <>
+      {total > 0 ? (
+        <ul className={styles.list}>
+          <li>Good: {good} </li>
+          <li>Neutral: {neutral} </li>
+          <li>Bad: {bad} </li>
+          <li>Total: {total} </li>
+          <li>Positive feedback: {Math.floor((good / total) * 100) || 0}%</li>
+        </ul>
+      ) : (
+        <Notification message="No feedback given" />
+      )}
+    </>
   );
 };
 Statistics.propTypes = {
